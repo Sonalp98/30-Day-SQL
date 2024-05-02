0424d8ba9edb11ed0a221e58e85af79d3514d905
Sonalp98 <165366403+Sonalp98@users.noreply.github.com>
2024-05-02 20:56:36 +0530

    Add files via upload

---
+++ Tiger_Analytics set of 2( flight origin and destination and new customer each month ).sql
@@ -0,0 +1,43 @@
+CREATE TABLE flights 
+(
+    cid VARCHAR(512),
+    fid VARCHAR(512),
+    origin VARCHAR(512),
+    Destination VARCHAR(512)
+);
+
+INSERT INTO flights (cid, fid, origin, Destination) VALUES ('1', 'f1', 'Del', 'Hyd');
+INSERT INTO flights (cid, fid, origin, Destination) VALUES ('1', 'f2', 'Hyd', 'Blr');
+INSERT INTO flights (cid, fid, origin, Destination) VALUES ('2', 'f3', 'Mum', 'Agra');
+INSERT INTO flights (cid, fid, origin, Destination) VALUES ('2', 'f4', 'Agra', 'Kol');
+select * from flights;
+select f.cid,f.origin,f2.Destination
+from flights f
+join flights f2 on f.Destination= f2.origin;
+drop table sales
+CREATE TABLE sales 
+(
+    order_date date,
+    customer VARCHAR(512),
+    qty INT
+);
+
+INSERT INTO sales (order_date, customer, qty) VALUES ('2021-01-01', 'C1', '20');
+INSERT INTO sales (order_date, customer, qty) VALUES ('2021-01-01', 'C2', '30');
+INSERT INTO sales (order_date, customer, qty) VALUES ('2021-02-01', 'C1', '10');
+INSERT INTO sales (order_date, customer, qty) VALUES ('2021-02-01', 'C3', '15');
+INSERT INTO sales (order_date, customer, qty) VALUES ('2021-03-01', 'C5', '19');
+INSERT INTO sales (order_date, customer, qty) VALUES ('2021-03-01', 'C4', '10');
+INSERT INTO sales (order_date, customer, qty) VALUES ('2021-04-01', 'C3', '13');
+INSERT INTO sales (order_date, customer, qty) VALUES ('2021-04-01', 'C5', '15');
+INSERT INTO sales (order_date, customer, qty) VALUES ('2021-04-01', 'C6', '10');
+select * from sales;
+-- Count of new customers added each month
+-- first visit of each customer
+select order_date , count(distinct customer) as no from(select order_date, customer, row_number() over(partition by customer order by order_date) as rn
+from sales ) s
+where rn=1
+group by order_date;
+select * from(select order_date, customer, row_number() over(partition by customer order by order_date) as rn
+from sales ) s
+where rn=1;
