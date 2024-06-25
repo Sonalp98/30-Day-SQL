d16d235b7e10f29a6c202ff1810538c764a312c1
Sonalp98 <165366403+Sonalp98@users.noreply.github.com>
2024-06-25 16:20:06 +0530

    Add files via upload

--- Consecutive days attendance.sql
+++ Consecutive days attendance.sql
@@ -43,4 +43,45 @@ select employee,first_value(dates) over(partition by employee,flag order by empl
 last_value(dates) over(partition by employee,flag order by employee,dates range between unbounded preceding and unbounded following) 
 as to_date,status
 from cte_absent
-order by employee,from_date
+order by employee,from_date;
+select * from emp_attendance;
+-- 2nd Solution
+With cte as(select *,row_number() over(partition by employee order by dates) as rn,
+row_number() over(partition by employee,status order by dates) as rn1
+from emp_attendance)
+select employee,min(dates) as from_date,max(dates) as to_date,status
+from cte
+group by employee,rn-rn1,status
+order by 1,2;
+-- 3rd solution
+With cte as(select *,row_number() over(partition by employee order by dates)-
+row_number() over(partition by employee,status order by dates) as rn
+from emp_attendance)
+select employee,min(dates) as from_date,max(dates) as to_date,status
+from cte
+group by employee,rn,status
+order by 1,2;
+-- 4th solution
+WITH CTE AS 
+	(SELECT *
+	, date_sub(dates, interval ROW_NUMBER() OVER(PARTITION BY employee, status ORDER BY dates) DAY) as date_grp
+	FROM emp_attendance)
+SELECT employee, MIN(dates) AS from_date 
+, MAX(dates) AS end_date, status
+FROM CTE
+GROUP BY employee, date_grp, status
+ORDER BY employee, from_date;
+-- 5th Solution
+WITH cte AS (
+    SELECT *, 
+           CASE WHEN status = LAG(status, 1, status) OVER (PARTITION BY employee ORDER BY dates) THEN 0 ELSE 1 END AS flag
+    FROM emp_attendance
+), 
+cte2 AS (
+    SELECT employee, dates, status, SUM(flag) OVER (PARTITION BY employee ORDER BY dates) AS flag_sum 
+    FROM cte
+) 
+SELECT employee, MIN(dates) AS from_date, MAX(dates) AS to_date, MAX(status) AS status
+FROM cte2
+GROUP BY employee, flag_sum
+ORDER BY employee, from_date;
